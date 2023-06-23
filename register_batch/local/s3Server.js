9353d0604e2cebcfc00f181e5b4138b2593f6f8a
laislopes <laisjkl@hotmail.com>
2023-06-23 17:23:00 -0300

    Uploading CSV File.

--- register_batch/local/s3Server.js
+++ register_batch/local/s3Server.js
@@ -1,4 +1,6 @@
 const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { readFile } = require("fs/promises");
+const { join } = require("path");
 
 function createLocalS3Client(){
     return new S3Client({
@@ -14,10 +16,14 @@ function createLocalS3Client(){
   async function uploadingToBucket() {
     const client = createLocalS3Client();
   
+    const fileName = "register_students.csv";
+    const filePath = join(__dirname, fileName);
+    const csvData = await readFile(filePath, "utf-8");
+
     const uploadCommand = new PutObjectCommand({
       Bucket: "students-csv-local",
-      Key: "test.csv",
-      Body: Buffer.from("12345")
+      Key: fileName,
+      Body: csvData
     })
   
     await client.send(uploadCommand); 
